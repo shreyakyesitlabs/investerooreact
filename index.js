e446378d45665c211f29006ab69ac0df36df1918
Shreyak Chakraborty <shreyak@yesitlabs.com>
2021-07-07 20:45:37 +0530

    fix port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express(); // create express app
 app.use(express.static("public"));
 
 // start express server on port 3000
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
